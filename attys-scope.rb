bcf9aa928120bc49eab13363f2673dc55b4f3872
Bernd Porr <bernd@glasgowneuro.tech>
2020-07-21 22:49:23 +0100

    Added the sha

--- attys-scope.rb
+++ attys-scope.rb
@@ -2,6 +2,7 @@ class AttysScope < Formula
   desc "Oscilloscope and data logger for the Attys DAQ (Linux/Windows/Mac)"
   homepage "http://www.attys.tech"
   url "https://github.com/glasgowneuro/attys-scope/archive/2.0.4.tar.gz"
+  sha256 "983f7170827c09cae9eaf097bfa06d0faf57b70ab31f2b102a348b7f7cc703f1"
 
   depends_on "qt5"
   depends_on "attys-comm"
