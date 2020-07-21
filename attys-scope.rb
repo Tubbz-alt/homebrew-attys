7bb3ea613ead9821b35ce3020af47beda08ce281
Bernd Porr <bernd@glasgowneuro.tech>
2020-07-21 22:42:36 +0100

    New version of attys-scope

--- attys-scope.rb
+++ attys-scope.rb
@@ -1,8 +1,7 @@
 class AttysScope < Formula
   desc "Oscilloscope and data logger for the Attys DAQ (Linux/Windows/Mac)"
   homepage "http://www.attys.tech"
-  url "https://github.com/glasgowneuro/attys-scope/archive/2.0.3.tar.gz"
-  sha256 "f96b6aabb5f967dfc7206c1cd3dd74013483de1a29bbf4ff2c8855c3dfb1b85e"
+  url "https://github.com/glasgowneuro/attys-scope/archive/2.0.4.tar.gz"
 
   depends_on "qt5"
   depends_on "attys-comm"
@@ -12,7 +11,6 @@ class AttysScope < Formula
     args = %W[-config release]
     system "qmake", "attys-scope.pro", *args
     system "make"
-    system "ls", "-l", "./attys-scope.app/Contents/MacOS/attys-scope"
     system "install", "-d", prefix/"bin/"
     system "install", "./attys-scope.app/Contents/MacOS/attys-scope", prefix/"bin/"
   end
