4f71e6056d0350362fc9e0c7ade75a3958846248
Bernd Porr <bernd@glasgowneuro.tech>
2020-07-26 00:27:45 +0100

    added sha

--- attys-ep.rb
+++ attys-ep.rb
@@ -2,6 +2,7 @@ class AttysEp < Formula
   desc "Visually evoked potentials / PSTH program for the Attys"
   homepage "http://www.attys.tech"
   url "https://github.com/glasgowneuro/attys-ep/archive/1.1.2.tar.gz"
+  sha256 "ab5c6ea9e6a2c1ec7711063bbaa2b2b765fef047e3c8e3a41012f83f4dfb78eb"
 
   depends_on "qt5"
   depends_on "attys-comm"
