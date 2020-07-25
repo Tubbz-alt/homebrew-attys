b49326adbb2269022990941e0df8d622003002b2
Bernd Porr <bernd@glasgowneuro.tech>
2020-07-26 00:05:36 +0100

    New version of attys-comm

--- attys-comm.rb
+++ attys-comm.rb
@@ -1,8 +1,8 @@
 class AttysComm < Formula
   desc "Attys C++ API"
   homepage "https://www.attys.tech/"
-  url "https://github.com/glasgowneuro/attys-comm/archive/2.0.13.tar.gz"
-  sha256 "f80590d87485429fafece162b8a6384591a37fdd866eb2c4b666013bd88ec2f4"
+  url "https://github.com/glasgowneuro/attys-comm/archive/2.0.14.tar.gz"
+
 
   depends_on "cmake" => :build
 
